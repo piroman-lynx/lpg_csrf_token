7f405ef46487616773711d92d1fb28a31cd3913f
Mikhail Konuhov <piroman@myconfig.ru>
2012-05-13 09:56:19 +0000

    fix depr

--- headers.function.php
+++ headers.function.php
@@ -2,8 +2,10 @@
 
 function getallheaders() {
     $headers = array();
-    foreach($_SERVER as $h=>$v)
-	if(ereg('HTTP_(.+)',$h,$hp))
-	    $headers[$hp[1]]=$v;
+    foreach($_SERVER as $h=>$v){
+	if(preg_match_all('/HTTP_(.+)/',$h,$hp)){
+	    $headers[$hp[1][0]]=$v;
+	}
+    }
     return $headers;
 }
