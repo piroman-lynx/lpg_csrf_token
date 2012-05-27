6e168db864dfa83c4dd634bbde4504105ef43b86
Mikhail Konuhov <piroman@myconfig.ru>
2012-05-27 18:43:17 +0000

    dynamic token with old token support

--- lpg.csrf.token.php
+++ lpg.csrf.token.php
@@ -1,8 +1,13 @@
 <?php
 
+function create_token($token_key){
+    //перемнные $_SERVER добавлены для повышения уникальности токена
+    $token = md5(time() . $_SERVER['REMOTE_ADDR']. $_SERVER['HTTP_USER_AGENT'] . $token_key);
+    return $token;
+}
 
 function lpg_csrf_token($token_key, $loading_text = "Loading...") {
-    if (defined(TOKEN_DISABLED)){
+    if (defined('TOKEN_DISABLED')){
 	return true;
     }
 
@@ -26,12 +31,38 @@ function lpg_csrf_token($token_key, $loading_text = "Loading...") {
     }
 
     if (($token == '') && ($method == 'ajax')) {
-        error_log("CSRF Attempt! Ajax atack form site: " . $_SERVER['HTTP_REFERER']);
+        error_log("[LPG_CSRF_TOKEN] Warning: CSRF Attempt! Ajax atack form site: " . $_SERVER['HTTP_REFERER']);
         return false;
     } else
     if (($source == 'header') && ($method == 'ajax')) {
-        if ($_SESSION['token_' . $token] !== true) {
-            error_log("CSRF Attempt! broken token from header/ajax");
+        if ($_SESSION['token_' . $token] === true) { //штатная ситуация - новый токен
+    	    //убиваем старый токен
+    	    foreach ($_SESSION as $name=>$value){
+    		if (preg_match('/^oldtoken_/',$name)){
+    		    unset($_SESSION[$name]);
+    		}
+    	    }
+
+    	    //ставим текущий как старый
+    	    $_SESSION['oldtoken_' . $token] = true;
+    	    unset($_SESSION['token_' . $token]);
+
+    	    //выбаем новый
+    	    $newtoken = create_token($token_key);
+    	    setcookie('XCSRFTOKEN',$newtoken,0,'/');
+    	    $_SESSION['token_'.$newtoken] = true;
+
+    	    return true;
+        } else if ($_SESSION['oldtoken_' . $token] === true) { //нештатаная ситуация - старый токен
+    	    error_log("[LPG_CSRF_TOKEN] Notice: old token");
+    	    //выдаем новый токен
+    	    $newtoken = create_token($token_key);
+    	    setcookie('XCSRFTOKEN',$newtoken,0,'/');
+    	    $_SERVER['token_'.$newtoken]=true;
+    	    
+    	    return true;
+        } else {
+    	    error_log("[LPG_CSRF_TOKEN] Warning: CSRF Attempt! broken token from header/ajax".var_export($_SESSION,true));
             return false;
         }
     } else
@@ -39,12 +70,12 @@ function lpg_csrf_token($token_key, $loading_text = "Loading...") {
         if ($_SESSION['token'] == $token) {
             $_SESSION['token_' . $token] = true;
         } else {
-            error_log("CSRF Attempt! broken token from get/classic");
+            error_log("[LPG_CSRF_TOKEN] Warning: CSRF Attempt! broken token from get/classic".var_export($_SESSION,true));
             return false;
         }
     } else
     if ($token == '') {
-        $token = md5(time() . $token_key);
+	$token = create_token($token_key);
         $_SESSION['token'] = $token;
         if (preg_match('/\?/i', $_SERVER['REQUEST_URI'])) {
             $token = "&csrf_token=" . $token;
--- token.js
+++ token.js
@@ -1,29 +1,41 @@
+function get_token(){
+    get = document.location.search.replace('?','&');
+    params = get.split('&');
+    for (i=0; i<params.length; i++){
+        param_vars = params[i].split('=');
+        if (param_vars[0]=='csrf_token'){
+            return param_vars[1];
+        }
+    }
+    if (console){
+        console.log('can\'t find token!');
+    }else{
+        alert('can\'t find token!');
+    }
+    return null;
+}
+
+function setup(){
+    $.ajaxSetup({
+	headers: {
+    	    'X-Csrf-Token':token
+	}
+    });
+}
+
+var token = get_token();
+
+setup();
 
 $(document).ready(function(){
-    
-    function get_token(){
-        get = document.location.search.replace('?','');
-        params = get.split('&');
-        for (i=0; i<params.length; i++){
-            param_vars = params[i].split('=');
-            if (param_vars[0]=='csrf_token'){
-                return param_vars[1];
-            }
-        }
-        if (console){
-            console.log('can\'t find token!');
-        }else{
-            alert('can\'t find token!');
-        }
-        return null;
-    }
-    
-    var token = get_token();
-    
-    $.ajaxSetup({
-        headers: {
-            'X-Csrf-Token':token
-        }
+    $('body').ajaxSuccess(function(){
+	cookies=document.cookie.split(';');
+	$.each(cookies, function(k,v){
+	    if (/XCSRFTOKEN/.test(v)){
+		s=v.split('=');
+		token = s[1];
+		setup();
+	    }
+	});
     });
-
 });
