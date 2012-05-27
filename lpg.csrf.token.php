718bf2d7695d8d89f6e754d086f0a455c25ae81b
Mikhail Konuhov <piroman@myconfig.ru>
2012-05-27 19:14:45 +0000

    fix

--- lpg.csrf.token.php
+++ lpg.csrf.token.php
@@ -1,12 +1,13 @@
 <?php
 
 function create_token($token_key){
+    $_SESSION['tokentime'] = time();
     //перемнные $_SERVER добавлены для повышения уникальности токена
     $token = md5(time() . $_SERVER['REMOTE_ADDR']. $_SERVER['HTTP_USER_AGENT'] . $token_key);
     return $token;
 }
 
-function lpg_csrf_token($token_key, $loading_text = "Loading...") {
+function lpg_csrf_token($token_key, $loading_text = "Loading...", $expire_time= 5) {
     if (defined('TOKEN_DISABLED')){
 	return true;
     }
@@ -36,24 +37,18 @@ function lpg_csrf_token($token_key, $loading_text = "Loading...") {
     } else
     if (($source == 'header') && ($method == 'ajax')) {
         if ($_SESSION['token_' . $token] === true) { //штатная ситуация - новый токен
-    	    //убиваем старый токен
-    	    foreach ($_SESSION as $name=>$value){
-    		if (preg_match('/^oldtoken_/',$name)){
-    		    unset($_SESSION[$name]);
-    		}
+    	    if ((int)$_SESSION['tokentime']+$expire_time < time()){
+    		//выдаем новый токен
+    		$newtoken = create_token($token_key);
+    		
+    		setcookie('XCSRFTOKEN',$newtoken,0,'/');
+    		$_SESSION['token_'.$newtoken] = true;
+    		
+    		$_SESSION['oldtoken'] = $token;
+    		//unset($_SESSION['token_' . $token]);
     	    }
-
-    	    //ставим текущий как старый
-    	    $_SESSION['oldtoken_' . $token] = true;
-    	    unset($_SESSION['token_' . $token]);
-
-    	    //выбаем новый
-    	    $newtoken = create_token($token_key);
-    	    setcookie('XCSRFTOKEN',$newtoken,0,'/');
-    	    $_SESSION['token_'.$newtoken] = true;
-
     	    return true;
-        } else if ($_SESSION['oldtoken_' . $token] === true) { //нештатаная ситуация - старый токен
+        } else if ($_SESSION['oldtoken'] == $token) { //нештатаная ситуация - старый токен
     	    error_log("[LPG_CSRF_TOKEN] Notice: old token");
     	    //выдаем новый токен
     	    $newtoken = create_token($token_key);
@@ -62,7 +57,7 @@ function lpg_csrf_token($token_key, $loading_text = "Loading...") {
     	    
     	    return true;
         } else {
-    	    error_log("[LPG_CSRF_TOKEN] Warning: CSRF Attempt! broken token from header/ajax".var_export($_SESSION,true));
+    	    error_log("[LPG_CSRF_TOKEN] Warning: CSRF Attempt! broken token from header/ajax");
             return false;
         }
     } else
@@ -70,7 +65,7 @@ function lpg_csrf_token($token_key, $loading_text = "Loading...") {
         if ($_SESSION['token'] == $token) {
             $_SESSION['token_' . $token] = true;
         } else {
-            error_log("[LPG_CSRF_TOKEN] Warning: CSRF Attempt! broken token from get/classic".var_export($_SESSION,true));
+            error_log("[LPG_CSRF_TOKEN] Warning: CSRF Attempt! broken token from get/classic");
             return false;
         }
     } else
